2a2eff76df8ba14ae7438790d7aa1b28f4d3b822
Md Ariful Islam <smarifulislami43@gmail.com>
2021-11-27 16:40:30 +0600

    make admin

--- index.js
+++ index.js
@@ -68,6 +68,28 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
+
+    //  UPDATE PUT API for admin
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      console.log(user.email);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    // api for getting admin info
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
   } finally {
     // await client.close();
   }
